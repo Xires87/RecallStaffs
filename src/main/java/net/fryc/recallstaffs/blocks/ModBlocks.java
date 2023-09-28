44988e8405b7855858d424d6cc54dca163249f34
Xires87 <frycmod@gmail.com>
2023-09-28 14:51:07 +0200

    textures, bug fixes and changed charging order (echo shards are first now)

--- src/main/java/net/fryc/recallstaffs/blocks/ModBlocks.java
+++ src/main/java/net/fryc/recallstaffs/blocks/ModBlocks.java
@@ -5,20 +5,24 @@ import net.fryc.recallstaffs.RecallStaffs;
 import net.fryc.recallstaffs.blocks.custom.RecoveryAltarBlock;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.MapColor;
-import net.minecraft.block.enums.Instrument;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 
 public class ModBlocks {
 
     public static final Block RECOVERY_ALTAR = registerBlock("recovery_altar",
-            new RecoveryAltarBlock(AbstractBlock.Settings.create().mapColor(MapColor.OAK_TAN).instrument(Instrument.BASS).strength(3f , 6f).sounds(BlockSoundGroup.GILDED_BLACKSTONE)));
+            new RecoveryAltarBlock(AbstractBlock.Settings.copy(Blocks.DEEPSLATE_TILES).nonOpaque()));
 
+    private static boolean never(BlockState blockState, BlockView blockView, BlockPos blockPos) {
+        return false;
+    }
 
     private static Block registerBlock(String name, Block block){
         registerBlockItem(name, block);
@@ -29,6 +33,8 @@ public class ModBlocks {
         return Registry.register(Registries.ITEM, new Identifier(RecallStaffs.MOD_ID, name),
                 new BlockItem(block, new FabricItemSettings()));
     }
+
+
     public static void registerModBlocks() {
     }
 }
