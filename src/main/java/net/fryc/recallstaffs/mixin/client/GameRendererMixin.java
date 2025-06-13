a9a7aa27b931555c3445f08b95b1c06126f0e39a
Xires87 <frycmod@gmail.com>
2025-06-13 10:38:36 +0200

    rendering portal effect when using staff
    (cherry picked from commit 829a4cc7e9ec05f448b435aeb51340f470707e71)

---
+++ src/main/java/net/fryc/recallstaffs/mixin/client/GameRendererMixin.java
@@ -0,0 +1,46 @@
+package net.fryc.recallstaffs.mixin.client;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.fryc.recallstaffs.items.custom.StaffItem;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(GameRenderer.class)
+abstract class GameRendererMixin implements AutoCloseable {
+
+    @Shadow
+    @Final MinecraftClient client;
+
+
+    @ModifyExpressionValue(
+            method = "render(Lnet/minecraft/client/render/RenderTickCounter;Z)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;hasStatusEffect(Lnet/minecraft/registry/entry/RegistryEntry;)Z")
+    )
+    private boolean renderNauseaEffectWhenUsingStaff(boolean original) {
+        // the place I'm injecting into is under 'this.client.player != null' check
+        return original || shouldRenderNauseaEffect(this.client.player);
+    }
+
+    @ModifyExpressionValue(
+            method = "renderWorld(Lnet/minecraft/client/render/RenderTickCounter;)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;hasStatusEffect(Lnet/minecraft/registry/entry/RegistryEntry;)Z")
+    )
+    private boolean setNauseaStrengthWhenUsingStaff(boolean original) {
+        if(this.client.player == null){
+            return original;
+        }
+        return original || shouldRenderNauseaEffect(this.client.player);
+    }
+
+    private static boolean shouldRenderNauseaEffect(PlayerEntity player){
+        return player.isUsingItem() && player.getActiveItem().getItem() instanceof StaffItem;
+    }
+
+}
